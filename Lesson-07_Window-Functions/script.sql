913598f0b740f7aea42e1c817edd543e6a96c296
Datago Academy <149325349+DataGo7@users.noreply.github.com>
2025-03-08 11:09:59 +0530

    Update script.sql

--- Lesson-07_Window-Functions/script.sql
+++ Lesson-07_Window-Functions/script.sql
@@ -1,4 +1,4 @@
--- Lesson-07: Window Functions (Modern HR Use Case)
+
 -- Rank employees by salary within each department
 SELECT EmpName, DeptID, Salary, RANK() OVER (PARTITION BY DeptID ORDER BY Salary DESC) AS Rank
 FROM Employee;
