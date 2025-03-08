564ec07231be068934794cfa626d082fee0ae70f
Datago Academy <149325349+DataGo7@users.noreply.github.com>
2025-03-08 11:11:21 +0530

    Update script.sql

--- Lesson-08_Stored-Procedures/script.sql
+++ Lesson-08_Stored-Procedures/script.sql
@@ -1,4 +1,4 @@
--- Lesson-08: Stored Procedures (Automation in Data Management)
+
 -- Automate employee salary updates based on department
 CREATE PROCEDURE UpdateSalaries(IN dept_name VARCHAR, IN percent DECIMAL)
 LANGUAGE SQL AS $$
